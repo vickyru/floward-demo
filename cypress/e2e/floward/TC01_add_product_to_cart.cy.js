bfb384d8036b564f257ba87a69cf9cf00515dac6
vickyru <prashant.0542@gmail.com>
2022-07-05 21:20:37 +0530

    few changes

--- cypress/e2e/floward/TC01_add_product_to_cart.cy.js
+++ cypress/e2e/floward/TC01_add_product_to_cart.cy.js
@@ -4,11 +4,15 @@ import "cypress-real-events/support";
 
 context('Verify product can be add to cart', () => {
 
-    before(()=>{
+    before(function (){
+        //access fixture data
+        cy.fixture('example').then(function(product){
+            this.product=product
+        })
         cy.visit('https://floward.com/')
     })
 
-    it('Add product to the cart', () => {
+    it('Add product to the cart', function () {
         //Verify that landing page
         cy.get('.landing-bg').should('be.visible')
 
@@ -22,12 +26,13 @@ context('Verify product can be add to cart', () => {
         .click()
 
         // Verify product is available then select it
-        cy.get('img[alt="Choco basket"]').as('product')
+        const productName = this.product.product_name
+        cy.get(`img[alt="${productName}"`).as('product')
         cy.get('@product').should('be.visible').realHover()
         cy.get('@product').parentsUntil('.image').parent().siblings().last().children().last().contains('Add to cart').click({force:true})
 
         // Verify cart if product added successfully
         cy.get('checkout-addons').siblings().last().find('button:contains(" Continue ")').should('be.visible').click({timeout:8000})
-        cy.get('.cart-item').find('h5.prod-name').should('contain',' Choco basket ')
+        cy.get('.cart-item').find('h5.prod-name').should('contain',productName)
     })
 })
