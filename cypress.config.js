d613d003a7e01806c6658151f4f9eeef809f1073
vickyru <prashant.0542@gmail.com>
2022-07-21 23:40:53 +0530

    reporter changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,21 @@ const { defineConfig } = require("cypress");
 const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  // reporter: 'cypress-mochawesome-reporter',
-  // reporterOptions: {
-  //   charts: true,
-  //   reportPageTitle: 'custom-title',
-  //   embeddedScreenshots: true,
-  //   inlineAssets: true,
-  //   saveAllAttempts: false
-  //   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+    },
 
   // Junit reporter
-  // "reporter": "junit",
-  // "reporterOptions": {
-  //    "mochaFile": "cypress/results/results.xml",
-  //    "toConsole": true,
-  // },
+  "reporter": "junit",
+  "reporterOptions": {
+     "mochaFile": "cypress/results/results.xml",
+     "toConsole": true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
         AllureWriter(on, config);
